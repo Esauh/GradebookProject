8a9264acff2f9174f34d97f93445151d6a7f82b3
Esauh <esau.hutcherson@bison.howard.edu>
2023-04-18 21:09:05 -0400

    finalized project

--- GradebookProject.sql
+++ GradebookProject.sql
@@ -1,6 +1,6 @@
-create database gradebook;
+create database final_project_demo;
 
-use gradebook;
+use final_project_demo;
 
 #Instantiates Course Table (PK: course_id)
 create table course(
@@ -39,8 +39,8 @@ foreign key (student_id) references Student(student_id));
 #Inserting one course into the Course Table
 insert into Course values(1, 'Electical Engineering', 200, 'Principles of Eletronics', 'Spring', 2023);
 
-#Un commenting and executing line below will all data inserted into Course Table  
-select * from Course
+#Un commenting and executing line below will show all data inserted into Course Table  
+select * from Course;
 
 #Inserting various students into students table
 insert into Student values
@@ -50,7 +50,7 @@ insert into Student values
 (4, 'Sally', 'Fullman',1);
 
 # Un commenting and executing line below will show all student data inserted into the Student Table
-#select * from Student
+#select * from Student;
 
 insert into Assignment values
 (1, 'Participation',10.0, 1),
@@ -63,7 +63,7 @@ insert into Assignment values
 
 #Inserting various grades into the Grade table for each student in our student table
 insert into Grade values
-#Grades for Darius (student_id=1)
+#Grades for Darius
 (1, 10.0, 1, 1),
 (2, 98.0, 2, 1),
 (3, 99.0, 3, 1),
@@ -85,13 +85,11 @@ insert into Grade values
 (16, 100.0, 4, 4);
 
 
-# Uncomment and execute line to see all grades in the grade table
+# Execute line to see all grades in the grade table
 select * from Grade;
 
 #Lists all the students in a course
 select s. * from Student s join Course c on s.course_id = c.course_id where c.course_id = 1;
-
-
 
 # lists all the students in the course along with their score from every assignment
 select Student.student_id, Student.first_name, Student.last_name, Assignment.assignment_id, Assignment.category, Grade.score
@@ -115,13 +113,20 @@ insert into Assignment values (5,'Midterm', 20.0, 1);
 #This will change the percentage of the category Tests to 25%
 update Assignment set percentage = 25.0 where category = 'Tests' and course_id = 1
 
-# This will add 2 points to the score of every student
-update Grade set score = score + 2 where assignment_id = '2';
+#select * from Assignment;
 
+# This will add 2 points to the score of every student
+update Grade set score = score + 2 where assignment_id = 2;
+
+select * from Grade where assignment_id = 2;
 
 #This will add 2 points to the score of students if their last name contains a Q
 update Grade join Student on Grade.student_id = Student.student_id
 set score = score + 2 where Student.last_name like '%Q%';
+
+#Add another test score for Darius
+insert into Grade values 
+(17, 50.0, 3, 1);
 
 #This will compute the grade of a student (student_id = 1 = Darius)
 select Student.first_name, Student.last_name, sum(Grade.score*Assignment.percentage)/sum(Assignment.percentage) as final_grade 
@@ -140,13 +145,12 @@ where Grade.score not in (
 group by Student.student_id;
 
 # Test Cases
-
 #Test 1 - Null values cannot be entered in certain attributes where not null is defined 
 insert into Student values (5, null, null, null);
 insert into Course values (2, null, null, null, 'Fall', 2022);
 
 
-#Test 2- Cannot insert an assignment into a course if the course_id does not exist
+# Test 2- Cannot insert an assignment into a course if the course_id does not exist
 insert into Assignment values(6, 'Group Project', 50  ,2);
 
 #Test 3 - If a student does not have grades entered his final grade should be null
@@ -154,3 +158,30 @@ insert into Student values (5, 'David', 'Blake', 1);
 select Student.first_name, Student.last_name, sum(Grade.score*Assignment.percentage)/sum(Assignment.percentage) as final_grade 
 from Student join Grade on Student.student_id = Grade.student_id
 join Assignment on Grade.assignment_id = Assignment.assignment_id where Student.student_id = 5;
+
+#Test 4- Testing final grade calculator correctly drops the final grade of a selected student (should return 100 as final grade since the grade of 50 is dropped)
+insert into Grade values (18, 100.0, 3, 5), (19,50.0,3,5);
+
+select Student.first_name, Student.last_name, sum(Assignment.percentage*Grade.score)/sum(Assignment.percentage) as final_grade 
+from Student join Grade on Student.student_id = Grade.student_id 
+join Assignment on Grade.assignment_id = Assignment.assignment_id 
+where Grade.score not in (
+    select min(Grade.score) 
+    from Grade join Assignment on Grade.assignment_id = Assignment.assignment_id 
+    where Assignment.category ='Tests') and Student.student_id = 5
+group by Student.student_id;
+
+#Test 5 - Checking that an added assignment is correctly added to the assignment table
+insert into Assignment values(6, 'Group Project', 50  ,1);
+select * from Assignment; 
+
+#Test 6- Checking that adding points to certain students with the letter Q in the last name works
+insert into Student values(6, 'Test', 'QName', 1);
+insert into Grade values (20, 50, 3, 6);
+update Grade join Student on Grade.student_id = Student.student_id
+set score = score + 2 where Student.last_name like '%Q%';
+select Student.student_id, Student.first_name, Student.last_name, Assignment.assignment_id, Assignment.category, Grade.score
+from Student join Course on Student.course_id = Course.course_id
+join Assignment on Course.course_id = Assignment.course_id
+left join Grade on Assignment.assignment_id = Grade.assignment_id and Student.student_id = Grade.student_id
+where Student.student_id=6;
--- TestCases.md
+++ TestCases.md
@@ -6,7 +6,7 @@
 insert into Student values (5, null, null, null);
 insert into Course values (2, null, null, null, 'Fall', 2022);
 ```
-Errors:Present (Test Passed) data cannot be entered due to null condition
+Errors:Present (Test Passed data not entered due to null condition) 
 ```
 20:30:05	insert into Student values (5, null, null, null)	Error Code: 1048. Column 'first_name' cannot be null	0.000 sec
 
@@ -19,7 +19,7 @@ Errors:Present (Test Passed) data cannot be entered due to null condition
 # Test 2- Cannot insert an assignment into a course if the course_id doesn't exist
 insert into Assignment values(6, 'Group Project', 50  ,2);
 ```
-Errors:Present (Test Passed)
+Errors:Present (Test Passed value assignment not entered)
 ```
 20:35:43	insert into Assignment values(6, 'Group Project', 50  ,2)	Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`testcases`.`assignment`, CONSTRAINT `assignment_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`))	0.015 sec
 ```
@@ -32,10 +32,64 @@ from Student join Grade on Student.student_id = Grade.student_id
 join Assignment on Grade.assignment_id = Assignment.assignment_id where Student.student_id = 5;
 ```
 
-Errors: Not Present (Test Passed)
+Errors: Not Present (Test Passed null row returned)
 ```
 3	29	20:42:47	select Student.first_name, Student.last_name, sum(Grade.score*Assignment.percentage)/sum(Assignment.percentage) as final_grade 
  from Student join Grade on Student.student_id = Grade.student_id
  join Assignment on Grade.assignment_id = Assignment.assignment_id where Student.student_id = 5
  LIMIT 0, 1000	1 row(s) returned	0.000 sec / 0.000 sec
 ```
+
+## **Case 4**
+```
+#Test 4- Testing final grade calculator correctly drops the final grade of a selected student (should return 100 as final grade since the grade of 50 is dropped)
+insert into Grade values (18, 100.0, 3, 5), (19,50.0,3,5);
+
+select Student.first_name, Student.last_name, sum(Assignment.percentage*Grade.score)/sum(Assignment.percentage) as final_grade 
+from Student join Grade on Student.student_id = Grade.student_id 
+join Assignment on Grade.assignment_id = Assignment.assignment_id 
+where Grade.score not in (
+    select min(Grade.score) 
+    from Grade join Assignment on Grade.assignment_id = Assignment.assignment_id 
+    where Assignment.category ='Tests') and Student.student_id = 5
+group by Student.student_id;
+```
+Errors:Not Present (Test Passed final score of 100 returned)
+```
+20:53:56	select Student.first_name, Student.last_name, sum(Assignment.percentage*Grade.score)/sum(Assignment.percentage) as final_grade  from Student join Grade on Student.student_id = Grade.student_id  join Assignment on Grade.assignment_id = Assignment.assignment_id  where Grade.score not in (     select min(Grade.score)      from Grade join Assignment on Grade.assignment_id = Assignment.assignment_id      where Assignment.category ='Tests') and Student.student_id = 5 group by Student.student_id LIMIT 0, 1000	1 row(s) returned	0.000 sec / 0.000 sec
+```
+
+## **Case 5**
+```
+#Test 5 - Checking that an added assignment is correctly added to the assignment table
+insert into Assignment values(6, 'Group Project', 50  ,1);
+select * from Assignment; 
+```
+Errors: None Present (Test Passed select statement queried and displayed the Group Project added to the Assignment table)
+```
+20:56:59	insert into Assignment values(6, 'Group Project', 50  ,1)	1 row(s) affected	0.000 sec
+
+20:59:42	select * from Assignment LIMIT 0, 1000	6 row(s) returned	0.000 sec / 0.000 sec
+```
+
+## **Case 6**
+```
+#Test 6- Checking that adding points to certain students with the letter Q in the last name works
+insert into Student values(6, 'Test', 'QName', 1);
+insert into Grade values (20, 50, 3, 6);
+update Grade join Student on Grade.student_id = Student.student_id
+set score = score + 2 where Student.last_name like '%Q%';
+select Student.student_id, Student.first_name, Student.last_name, Assignment.assignment_id, Assignment.category, Grade.score
+from Student join Course on Student.course_id = Course.course_id
+join Assignment on Course.course_id = Assignment.course_id
+left join Grade on Assignment.assignment_id = Grade.assignment_id and Student.student_id = Grade.student_id
+where Student.student_id=6;
+```
+Errors: None Present (Test Passed Test QName score in Tests is 52 after score was incremented by 2 and not the original 50 that it was set as)
+```
+21:04:15	update Grade join Student on Grade.student_id = Student.student_id set score = score + 2 where Student.last_name like '%Q%'	6 row(s) affected Rows matched: 6  Changed: 6  Warnings: 0	0.015 sec
+
+21:04:18	select Student.student_id, Student.first_name, Student.last_name, Assignment.assignment_id, Assignment.category, Grade.score from Student join Course on Student.course_id = Course.course_id join Assignment on Course.course_id = Assignment.course_id left join Grade on Assignment.assignment_id = Grade.assignment_id and Student.student_id = Grade.student_id where Student.student_id=6 LIMIT 0, 1000	6 row(s) returned	0.000 sec / 0.000 sec
+```
+
+# **All relevant Test Passed!**
